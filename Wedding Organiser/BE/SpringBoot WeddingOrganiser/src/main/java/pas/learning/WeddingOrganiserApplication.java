d8765f49daa1fd23db9a44aeae220d8cbecf3de1
sebastianzelazny <tassel_xxx112@interia.pl>
2020-11-13 22:51:13 +0100

    Adding BE App

---
+++ Wedding Organiser/BE/SpringBoot WeddingOrganiser/src/main/java/pas/learning/WeddingOrganiserApplication.java
@@ -0,0 +1,13 @@
+package pas.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeddingOrganiserApplication.class, args);
+	}
+
+}
---
+++ Wedding Organiser/BE/SpringBoot WeddingOrganiser/src/test/java/pas/learning/WeddingOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package pas.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingOrganiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
