0ae8947a2a466873210e5d22dc0175e0d324b934
unknown <piotrek_x@vp.pl>
2020-12-01 21:11:28 +0100

    db schema last update

--- Wedding Organiser/DB/DBSchema.sql
+++ Wedding Organiser/DB/DBSchema.sql
@@ -19,10 +19,8 @@ CREATE TABLE `Address` (
   `postal_code` varchar(255) NOT NULL,
   `street` varchar(255) NOT NULL,
   `additional_comment` varchar(5000) DEFAULT NULL,
-  PRIMARY KEY (`address_id`),
-  CONSTRAINT `Address_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `Service` (`address_id`),
-  CONSTRAINT `Address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `Guest` (`address_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Budget` (
   `budget_id` int NOT NULL AUTO_INCREMENT,
@@ -32,15 +30,16 @@ CREATE TABLE `Budget` (
   PRIMARY KEY (`budget_id`),
   UNIQUE KEY `event_id` (`event_id`),
   CONSTRAINT `Budget_FK` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Contact` (
   `contact_id` int NOT NULL AUTO_INCREMENT,
   `phone` varchar(20) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `facebook` varchar(255) DEFAULT NULL,
+  `contact_type` varchar(10) NOT NULL,
   PRIMARY KEY (`contact_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Event` (
   `event_id` int NOT NULL AUTO_INCREMENT,
@@ -52,14 +51,13 @@ CREATE TABLE `Event` (
   UNIQUE KEY `address_id` (`address_id`),
   KEY `account_id` (`account_id`),
   CONSTRAINT `Event_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Account` (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Event_expense` (
   `event_expense_id` int NOT NULL AUTO_INCREMENT,
   `budget_id` int NOT NULL,
   `expense_id` int DEFAULT NULL,
   `service_id` int DEFAULT NULL,
-  `event_service_id` int DEFAULT NULL,
   `description` varchar(5000) DEFAULT NULL,
   `expense_type` varchar(10) NOT NULL,
   `cost` decimal(10,0) DEFAULT NULL,
@@ -73,7 +71,7 @@ CREATE TABLE `Event_expense` (
   CONSTRAINT `Event_expense_ibfk_1` FOREIGN KEY (`budget_id`) REFERENCES `Budget` (`budget_id`),
   CONSTRAINT `Event_expense_ibfk_2` FOREIGN KEY (`expense_id`) REFERENCES `Expense` (`expense_id`),
   CONSTRAINT `Event_expense_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Event_todo` (
   `event_todo_id` int NOT NULL AUTO_INCREMENT,
@@ -89,26 +87,26 @@ CREATE TABLE `Event_todo` (
   KEY `todo_id` (`todo_id`),
   CONSTRAINT `Event_todo_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`),
   CONSTRAINT `Event_todo_ibfk_2` FOREIGN KEY (`todo_id`) REFERENCES `ToDo` (`todo_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Expense` (
   `expense_id` int NOT NULL AUTO_INCREMENT,
   `description` varchar(5000) DEFAULT NULL,
   `expense_type` varchar(10) NOT NULL,
   PRIMARY KEY (`expense_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `File` (
   `file_id` int NOT NULL AUTO_INCREMENT,
   `gallery_id` int NOT NULL,
   `file_type` varchar(10) NOT NULL,
-  `file_size` mediumtext NOT NULL,
+  `file_size` int NOT NULL,
   `file_description` varchar(5000) DEFAULT NULL,
   `upload_date` datetime DEFAULT NULL,
   PRIMARY KEY (`file_id`),
   KEY `gallery_id` (`gallery_id`),
   CONSTRAINT `File_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `File_guest_tag` (
   `file_guest_tag_id` int NOT NULL AUTO_INCREMENT,
@@ -119,7 +117,7 @@ CREATE TABLE `File_guest_tag` (
   KEY `guest_id` (`guest_id`),
   CONSTRAINT `File_guest_tag_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`),
   CONSTRAINT `File_guest_tag_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `File_tags` (
   `file_tags_id` int NOT NULL AUTO_INCREMENT,
@@ -128,7 +126,7 @@ CREATE TABLE `File_tags` (
   PRIMARY KEY (`file_tags_id`),
   KEY `file_id` (`file_id`),
   CONSTRAINT `File_tags_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Gallery` (
   `gallery_id` int NOT NULL AUTO_INCREMENT,
@@ -139,7 +137,7 @@ CREATE TABLE `Gallery` (
   PRIMARY KEY (`gallery_id`),
   KEY `event_id` (`event_id`),
   CONSTRAINT `Gallery_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Gallery_access` (
   `gallery_access_id` int NOT NULL AUTO_INCREMENT,
@@ -150,7 +148,7 @@ CREATE TABLE `Gallery_access` (
   KEY `guest_id` (`guest_id`),
   CONSTRAINT `Gallery_access_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`),
   CONSTRAINT `Gallery_access_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Guest` (
   `guest_id` int NOT NULL AUTO_INCREMENT,
@@ -173,25 +171,25 @@ CREATE TABLE `Guest` (
   UNIQUE KEY `contact_id` (`contact_id`),
   UNIQUE KEY `address_id` (`address_id`),
   KEY `event_id` (`event_id`),
+  CONSTRAINT `Guest_FK` FOREIGN KEY (`address_id`) REFERENCES `Address` (`address_id`),
   CONSTRAINT `Guest_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`),
   CONSTRAINT `Guest_ibfk_2` FOREIGN KEY (`contact_id`) REFERENCES `Contact` (`contact_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Guest_table` (
   `guest_table_id` int NOT NULL AUTO_INCREMENT,
-  `table_id` int NOT NULL,
+  `table_definition_id` int NOT NULL,
   `guest_id` int NOT NULL,
   `position` int DEFAULT NULL,
   PRIMARY KEY (`guest_table_id`),
   UNIQUE KEY `guest_id` (`guest_id`),
-  KEY `table_id` (`table_id`),
-  CONSTRAINT `Guest_table_ibfk_1` FOREIGN KEY (`table_id`) REFERENCES `Table` (`table_id`),
+  KEY `table_id` (`table_definition_id`),
+  CONSTRAINT `Guest_table_ibfk_1` FOREIGN KEY (`table_definition_id`) REFERENCES `Table_definition` (`table_definition_id`),
   CONSTRAINT `Guest_table_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Service` (
   `service_id` int NOT NULL AUTO_INCREMENT,
-  `address_id` int DEFAULT NULL,
   `contact_id` int NOT NULL,
   `service_name` varchar(500) NOT NULL,
   `service_type` varchar(10) NOT NULL,
@@ -201,9 +199,8 @@ CREATE TABLE `Service` (
   `currency` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`service_id`),
   UNIQUE KEY `contact_id` (`contact_id`),
-  UNIQUE KEY `address_id` (`address_id`),
   CONSTRAINT `Service_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `Contact` (`contact_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `Service_address` (
   `service_address_id` int NOT NULL AUTO_INCREMENT,
@@ -214,19 +211,19 @@ CREATE TABLE `Service_address` (
   KEY `service_id` (`service_id`),
   CONSTRAINT `Service_address_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`),
   CONSTRAINT `Service_address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `Address` (`address_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `Table` (
-  `table_id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `Table_definition` (
+  `table_definition_id` int NOT NULL AUTO_INCREMENT,
   `event_id` int NOT NULL,
   `table_name` varchar(100) NOT NULL,
   `table_type` varchar(10) NOT NULL,
   `table_size` int NOT NULL,
   `additional_comment` varchar(5000) DEFAULT NULL,
-  PRIMARY KEY (`table_id`),
+  PRIMARY KEY (`table_definition_id`),
   KEY `event_id` (`event_id`),
-  CONSTRAINT `Table_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  CONSTRAINT `Table_definition_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `ToDo` (
   `todo_id` int NOT NULL AUTO_INCREMENT,
@@ -234,4 +231,4 @@ CREATE TABLE `ToDo` (
   `description` varchar(5000) NOT NULL,
   `priority` int NOT NULL,
   PRIMARY KEY (`todo_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
