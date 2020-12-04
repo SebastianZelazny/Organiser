5ee1960f171d2fa881b3c7382255d26d54a31aec
piotrekradomski <piotrek_x@vp.pl>
2020-12-04 22:35:49 +0100

    updated DB schema

--- Wedding Organiser/DB/DBSchema.sql
+++ Wedding Organiser/DB/DBSchema.sql
@@ -59,50 +59,6 @@ CREATE TABLE `Account` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- WeddingOrganizer.Service definition
-
-CREATE TABLE `Service` (
-  `service_id` int NOT NULL AUTO_INCREMENT,
-  `contact_id` int NOT NULL,
-  `service_name` varchar(500) NOT NULL,
-  `service_type` varchar(10) NOT NULL,
-  `description` varchar(5000) DEFAULT NULL,
-  `cost_min` decimal(10,0) DEFAULT NULL,
-  `cost_max` decimal(10,0) DEFAULT NULL,
-  `currency` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`service_id`),
-  UNIQUE KEY `contact_id` (`contact_id`),
-  CONSTRAINT `Service_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `Contact` (`contact_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.Service_address definition
-
-CREATE TABLE `Service_address` (
-  `service_address_id` int NOT NULL AUTO_INCREMENT,
-  `address_id` int NOT NULL,
-  `service_id` int NOT NULL,
-  PRIMARY KEY (`service_address_id`),
-  UNIQUE KEY `address_id` (`address_id`),
-  KEY `service_id` (`service_id`),
-  CONSTRAINT `Service_address_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`),
-  CONSTRAINT `Service_address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `Address` (`address_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.Budget definition
-
-CREATE TABLE `Budget` (
-  `budget_id` int NOT NULL AUTO_INCREMENT,
-  `event_id` int NOT NULL,
-  `value` decimal(10,0) DEFAULT NULL,
-  `currency` varchar(10) DEFAULT NULL,
-  PRIMARY KEY (`budget_id`),
-  UNIQUE KEY `event_id` (`event_id`),
-  CONSTRAINT `Budget_FK` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
 -- WeddingOrganizer.Event definition
 
 CREATE TABLE `Event` (
@@ -116,29 +72,6 @@ CREATE TABLE `Event` (
   KEY `account_id` (`account_id`),
   CONSTRAINT `Event_FK` FOREIGN KEY (`address_id`) REFERENCES `Address` (`address_id`),
   CONSTRAINT `Event_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `Account` (`account_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.Event_expense definition
-
-CREATE TABLE `Event_expense` (
-  `event_expense_id` int NOT NULL AUTO_INCREMENT,
-  `budget_id` int NOT NULL,
-  `expense_id` int DEFAULT NULL,
-  `service_id` int DEFAULT NULL,
-  `description` varchar(5000) DEFAULT NULL,
-  `expense_type` varchar(10) NOT NULL,
-  `cost` decimal(10,0) DEFAULT NULL,
-  `paid` decimal(10,0) DEFAULT NULL,
-  `currency` varchar(10) DEFAULT NULL,
-  `additional_comment` varchar(5000) DEFAULT NULL,
-  PRIMARY KEY (`event_expense_id`),
-  KEY `budget_id` (`budget_id`),
-  KEY `expense_id` (`expense_id`),
-  KEY `service_id` (`service_id`),
-  CONSTRAINT `Event_expense_ibfk_1` FOREIGN KEY (`budget_id`) REFERENCES `Budget` (`budget_id`),
-  CONSTRAINT `Event_expense_ibfk_2` FOREIGN KEY (`expense_id`) REFERENCES `Expense` (`expense_id`),
-  CONSTRAINT `Event_expense_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -161,47 +94,6 @@ CREATE TABLE `Event_todo` (
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- WeddingOrganizer.File definition
-
-CREATE TABLE `File` (
-  `file_id` int NOT NULL AUTO_INCREMENT,
-  `gallery_id` int NOT NULL,
-  `file_type` varchar(10) NOT NULL,
-  `file_size` int NOT NULL,
-  `file_description` varchar(5000) DEFAULT NULL,
-  `upload_date` datetime DEFAULT NULL,
-  PRIMARY KEY (`file_id`),
-  KEY `gallery_id` (`gallery_id`),
-  CONSTRAINT `File_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.File_guest_tag definition
-
-CREATE TABLE `File_guest_tag` (
-  `file_guest_tag_id` int NOT NULL AUTO_INCREMENT,
-  `file_id` int NOT NULL,
-  `guest_id` int NOT NULL,
-  PRIMARY KEY (`file_guest_tag_id`),
-  KEY `file_id` (`file_id`),
-  KEY `guest_id` (`guest_id`),
-  CONSTRAINT `File_guest_tag_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`),
-  CONSTRAINT `File_guest_tag_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.File_tags definition
-
-CREATE TABLE `File_tags` (
-  `file_tags_id` int NOT NULL AUTO_INCREMENT,
-  `file_id` int NOT NULL,
-  `hashtag` varchar(500) NOT NULL,
-  PRIMARY KEY (`file_tags_id`),
-  KEY `file_id` (`file_id`),
-  CONSTRAINT `File_tags_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
 -- WeddingOrganizer.Gallery definition
 
 CREATE TABLE `Gallery` (
@@ -213,20 +105,6 @@ CREATE TABLE `Gallery` (
   PRIMARY KEY (`gallery_id`),
   KEY `event_id` (`event_id`),
   CONSTRAINT `Gallery_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
--- WeddingOrganizer.Gallery_access definition
-
-CREATE TABLE `Gallery_access` (
-  `gallery_access_id` int NOT NULL AUTO_INCREMENT,
-  `gallery_id` int NOT NULL,
-  `guest_id` int NOT NULL,
-  PRIMARY KEY (`gallery_access_id`),
-  KEY `gallery_id` (`gallery_id`),
-  KEY `guest_id` (`guest_id`),
-  CONSTRAINT `Gallery_access_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`),
-  CONSTRAINT `Gallery_access_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
@@ -259,19 +137,35 @@ CREATE TABLE `Guest` (
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
--- WeddingOrganizer.Guest_table definition
+-- WeddingOrganizer.Service definition
 
-CREATE TABLE `Guest_table` (
-  `guest_table_id` int NOT NULL AUTO_INCREMENT,
-  `table_definition_id` int NOT NULL,
-  `guest_id` int NOT NULL,
-  `position` int DEFAULT NULL,
-  PRIMARY KEY (`guest_table_id`),
-  UNIQUE KEY `guest_id` (`guest_id`),
-  KEY `table_id` (`table_definition_id`),
-  CONSTRAINT `Guest_table_ibfk_1` FOREIGN KEY (`table_definition_id`) REFERENCES `Table_definition` (`table_definition_id`),
-  CONSTRAINT `Guest_table_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
+CREATE TABLE `Service` (
+  `service_id` int NOT NULL AUTO_INCREMENT,
+  `contact_id` int NOT NULL,
+  `service_name` varchar(500) NOT NULL,
+  `service_type` varchar(10) NOT NULL,
+  `description` varchar(5000) DEFAULT NULL,
+  `cost_min` decimal(10,0) DEFAULT NULL,
+  `cost_max` decimal(10,0) DEFAULT NULL,
+  `currency` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`service_id`),
+  UNIQUE KEY `contact_id` (`contact_id`),
+  CONSTRAINT `Service_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `Contact` (`contact_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.Service_address definition
+
+CREATE TABLE `Service_address` (
+  `service_address_id` int NOT NULL AUTO_INCREMENT,
+  `address_id` int NOT NULL,
+  `service_id` int NOT NULL,
+  PRIMARY KEY (`service_address_id`),
+  UNIQUE KEY `address_id` (`address_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `Service_address_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`),
+  CONSTRAINT `Service_address_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `Address` (`address_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 -- WeddingOrganizer.Table_definition definition
@@ -286,4 +180,110 @@ CREATE TABLE `Table_definition` (
   PRIMARY KEY (`table_definition_id`),
   KEY `event_id` (`event_id`),
   CONSTRAINT `Table_definition_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.Budget definition
+
+CREATE TABLE `Budget` (
+  `budget_id` int NOT NULL AUTO_INCREMENT,
+  `event_id` int NOT NULL,
+  `value` decimal(10,0) DEFAULT NULL,
+  `currency` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`budget_id`),
+  UNIQUE KEY `event_id` (`event_id`),
+  CONSTRAINT `Budget_FK` FOREIGN KEY (`event_id`) REFERENCES `Event` (`event_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.Event_expense definition
+
+CREATE TABLE `Event_expense` (
+  `event_expense_id` int NOT NULL AUTO_INCREMENT,
+  `budget_id` int NOT NULL,
+  `expense_id` int DEFAULT NULL,
+  `service_id` int DEFAULT NULL,
+  `description` varchar(5000) DEFAULT NULL,
+  `expense_type` varchar(10) NOT NULL,
+  `cost` decimal(10,0) DEFAULT NULL,
+  `paid` decimal(10,0) DEFAULT NULL,
+  `currency` varchar(10) DEFAULT NULL,
+  `additional_comment` varchar(5000) DEFAULT NULL,
+  PRIMARY KEY (`event_expense_id`),
+  KEY `budget_id` (`budget_id`),
+  KEY `expense_id` (`expense_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `Event_expense_ibfk_1` FOREIGN KEY (`budget_id`) REFERENCES `Budget` (`budget_id`),
+  CONSTRAINT `Event_expense_ibfk_2` FOREIGN KEY (`expense_id`) REFERENCES `Expense` (`expense_id`),
+  CONSTRAINT `Event_expense_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `Service` (`service_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.File definition
+
+CREATE TABLE `File` (
+  `file_id` int NOT NULL AUTO_INCREMENT,
+  `gallery_id` int NOT NULL,
+  `file_type` varchar(10) NOT NULL,
+  `file_size` int NOT NULL,
+  `file_description` varchar(5000) DEFAULT NULL,
+  `upload_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`file_id`),
+  KEY `gallery_id` (`gallery_id`),
+  CONSTRAINT `File_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.File_guest_tag definition
+
+CREATE TABLE `File_guest_tag` (
+  `file_guest_tag_id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int NOT NULL,
+  `guest_id` int NOT NULL,
+  PRIMARY KEY (`file_guest_tag_id`),
+  KEY `file_id` (`file_id`),
+  KEY `guest_id` (`guest_id`),
+  CONSTRAINT `File_guest_tag_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`),
+  CONSTRAINT `File_guest_tag_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.File_tag definition
+
+CREATE TABLE `File_tag` (
+  `file_tag_id` int NOT NULL AUTO_INCREMENT,
+  `file_id` int NOT NULL,
+  `hashtag` varchar(500) NOT NULL,
+  PRIMARY KEY (`file_tag_id`),
+  KEY `file_id` (`file_id`),
+  CONSTRAINT `File_tag_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `File` (`file_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.Gallery_access definition
+
+CREATE TABLE `Gallery_access` (
+  `gallery_access_id` int NOT NULL AUTO_INCREMENT,
+  `gallery_id` int NOT NULL,
+  `guest_id` int NOT NULL,
+  PRIMARY KEY (`gallery_access_id`),
+  KEY `gallery_id` (`gallery_id`),
+  KEY `guest_id` (`guest_id`),
+  CONSTRAINT `Gallery_access_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `Gallery` (`gallery_id`),
+  CONSTRAINT `Gallery_access_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- WeddingOrganizer.Guest_table definition
+
+CREATE TABLE `Guest_table` (
+  `guest_table_id` int NOT NULL AUTO_INCREMENT,
+  `table_definition_id` int NOT NULL,
+  `guest_id` int NOT NULL,
+  `position` int DEFAULT NULL,
+  PRIMARY KEY (`guest_table_id`),
+  UNIQUE KEY `guest_id` (`guest_id`),
+  KEY `table_id` (`table_definition_id`),
+  CONSTRAINT `Guest_table_ibfk_1` FOREIGN KEY (`table_definition_id`) REFERENCES `Table_definition` (`table_definition_id`),
+  CONSTRAINT `Guest_table_ibfk_2` FOREIGN KEY (`guest_id`) REFERENCES `Guest` (`guest_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
