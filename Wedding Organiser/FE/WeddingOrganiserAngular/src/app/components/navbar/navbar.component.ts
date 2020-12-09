db8ca3b85692884f1db4744d768e237c343d57d0
sebastianzelazny <tassel_xxx112@interia.pl>
2020-12-09 22:26:34 +0100

    Adding New NavBar to Page

---
+++ Wedding Organiser/FE/WeddingOrganiserAngular/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.hello2();
+  }
+
+  hello2(){
+    alert("aadddad");
+  }
+  
+}
